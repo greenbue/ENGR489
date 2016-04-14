5c1f2efdacd21a6b9d007af17fbb218a306e4ff5
greenbue <generyan.ebue@gmail.com>
2016-04-15 11:16:59 +1200

    tab js fix

--- web/js/tab.js
+++ web/js/tab.js
@@ -3,17 +3,11 @@ $(document).ready(function(){
         $(this).tab('show');
     });
     $(function() {
-      if ( document.location.href.indexOf('#home') > -1 ) {
-        $('#home_tab').click();
+      if ( document.location.href.indexOf('#summary') > -1 ) {
+        $('#summary_tab').click();
       }
-      else if ( document.location.href.indexOf('#about') > -1 ) {
-        $('#about_tab').click();
-      }
-      else if ( document.location.href.indexOf('#services') > -1 ) {
-        $('#services_tab').click();
-      }
-      else if ( document.location.href.indexOf('#testimonials') > -1 ) {
-        $('#testimonials_tab').click();
+      else if ( document.location.href.indexOf('#game_per') > -1 ) {
+        $('#game_per_tab').click();
       }
     });
     $('.navbar-nav a').on('shown.bs.tab', function(event){
