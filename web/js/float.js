8f45591de1b24626a9525168678965d33a7df8bd
greenbue <generyan.ebue@gmail.com>
2016-05-04 14:59:54 +1200

    draggable and floating menu

---
+++ web/js/float.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    titleloc = parseInt($('.sidebar-title').css("top").substring(0,$('.sidebar-title').css("top").indexOf("px")))
+		menuloc = parseInt($('.sidebar').css("top").substring(0,$('.sidebar').css("top").indexOf("px")))
+    $(window).scroll(function () { 
+			var title_offset = titleloc+$(document).scrollTop()+"px";
+			$('.sidebar-title').animate({top:title_offset},{duration:300,queue:false});
+			var menu_offset = menuloc+$(document).scrollTop()+"px";
+			$('.sidebar').animate({top:menu_offset},{duration:300,queue:false});
+    });
+});
+
+$(function() {
+		$( ".draggable" ).draggable();
+});
