d89b91a7055c6bf57892cf5f7b1ea9789b61837f
greenbue <generyan.ebue@gmail.com>
2016-09-16 14:43:00 +1200

    automate guide if it's the first time.

--- web/js/cricket.js
+++ web/js/cricket.js
@@ -1526,3 +1526,25 @@ function brush_on2() {
   $('#brush_on2').val((result_year_chart2.brushOn() == true) ? "Turn OFF Brush filter" : "Turn ON Brush filter");
   dc.renderAll();
 }
+
+function setCookie(cname, cvalue, exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  var expires = "expires="+ d.toUTCString();
+  document.cookie = cname + "=" + cvalue + "; " + expires;
+}
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var ca = document.cookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0)==' ') {
+          c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+          return c.substring(name.length,c.length);
+      }
+  }
+  return "";
+}
